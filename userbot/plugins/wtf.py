1c24ff1d396c10433757d9073dd77d79d98aee6e
japarmar <55135328+japarmar@users.noreply.github.com>
2020-02-24 18:56:42 +0530

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -10,24 +10,22 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("(.*)"))
+@borg.on(admin_cmd("wtf"))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.3
     animation_ttl = range(0, 5)
-    input_str = event.pattern_match.group(1)
-    if input_str == "wtf1":
-        await event.edit(input_str)
-        animation_chars = [
+    await event.edit("wtf")
+    animation_chars = [
             "What",
             "What The",
             "What The F",
             "What The F Brah",
-            "What The F Brah(https://telegra.ph//file/f3b760e4a99340d331f9b.jpg)"
+            "[What The F Brah](https://telegra.ph//file/f3b760e4a99340d331f9b.jpg)"
         ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i %5 ])
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i %5 ])
