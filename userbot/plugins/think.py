a7fcb532f2a1c3de57aaf0acea85c901fc8a8c12
japarmar <55135328+japarmar@users.noreply.github.com>
2020-02-24 18:54:49 +0530

    Update think.py

--- userbot/plugins/think.py
+++ userbot/plugins/think.py
@@ -10,16 +10,15 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("(.*)"))
+@borg.on(admin_cmd("think"))
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.01
+    animation_interval = 0.1
     animation_ttl = range(0, 288)
-    input_str = event.pattern_match.group(1)
-    if input_str == "think":
-        await event.edit(input_str)
-        animation_chars = [
+    
+    await event.edit(input_str)
+    animation_chars = [
             "THINKING",
             "THI&K#N₹",
             "T+IN@I?G",
@@ -58,7 +57,7 @@ async def _(event):
             "THINKING... 🤔"
         ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 72])
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 72])
