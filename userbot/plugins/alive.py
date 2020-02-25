7dd9bdc194c0923287b1af8f8032a4b3f8dcddec
japarmar <55135328+japarmar@users.noreply.github.com>
2020-02-25 12:11:04 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,7 +10,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nmy peru master:` {DEFAULTUSER}\nuserbot: [GujjuBot](https://www.github.com/GujjuBot)\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
+    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nMy peru master: {DEFAULTUSER} \nUserbot:` [GujjuBot](https://www.github.com/GujjuBot)\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
