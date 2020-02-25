6f381256f3f6ccb1c9fab69aa7ba4a964e6e29d6
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-02-25 12:19:08 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,23 +1,18 @@
-""".admin Plugin for @UniBorg"""
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from userbot.utils import admin_cmd
+from platform import uname
 from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-@borg.on(admin_cmd("alive"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nMy peru master: {DEFAULTUSER} \nUserbot:` [GujjuBot](https://www.github.com/GujjuBot)\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n"
+                     f"`My peru owner`: {DEFAULTUSER}\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Halto_Tha\n"
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     "[Deploy this userbot Now](https://github.com/japarmar/GujjuBot)")
