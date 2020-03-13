666d57b4703fd55b96b033e7feec2cc3d902b010
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-13 21:52:25 +0530

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -18,7 +18,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"{HM} ADNAN {DM}"
+        name = f"{HM} Jayu {DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
