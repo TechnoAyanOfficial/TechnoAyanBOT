e23fcbd3788f2a0ba4ef6de0aa5858459871b468
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-15 00:51:31 +0530

    Update audio.py

--- userbot/plugins/audio.py
+++ userbot/plugins/audio.py
@@ -5,7 +5,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd("au ?(.*)"))
+@borg.on(admin_cmd("audio ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
