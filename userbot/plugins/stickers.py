92e52ea742887d7c7362013c575c31c8ea9fcc45
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-15 23:09:03 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -47,11 +47,11 @@ async def _(event):
     #packname = f"JayukeStickers"
     #packshortname = f"GujjuBot_{userid}_ns"  # format: Uni_Borg_userid
     if userid == 948408212:
-            packname = f"JayukeStickers"
-            packshortname = "Jayu_ke_locker_me"
-        else:
-            packname = f"GujjuBot{userid}"
-            packshortname = f"GujjuBot_{userid}_ns"
+        packname = f"JayukeStickers"
+        packshortname = "Jayu_ke_locker_me"
+    else:
+        packname = f"GujjuBot{userid}"
+        packshortname = f"GujjuBot_{userid}_ns"
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
