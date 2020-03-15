091d267524fd3422338846f155126102234a5433
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-15 22:55:16 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"JayukeStickers"
-    packshortname = f"Jayu_ke_locker_me"  # format: Uni_Borg_userid
+    packname = f"GujjuBot{userid}"
+    packshortname = f"GujjuBot_{userid}_ns"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
@@ -54,11 +54,11 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"JayukeAnimated"
-        if userid == 719877937:
+        packname = f"{userid}'s @AnimatedStickersGroup"
+        if userid == 948408212:
             packshortname = "Jayu_Animated"
         else:
-            packshortname = f"Jayu_Animated" # format: Uni_Borg_userid
+            packshortname = f"GujjuBot_{userid}_an" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
