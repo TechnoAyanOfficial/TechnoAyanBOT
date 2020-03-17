59417b7c0a24d83ded18c16aec81d712c344eb5e
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-17 19:18:48 +0530

    Update schd_spam.py

--- userbot/plugins/schd_spam.py
+++ userbot/plugins/schd_spam.py
@@ -12,7 +12,7 @@ async def spammer(e):
         counter = int(text[1])
         spam_message = str(text[2])
         sleep2 = int(text[3])
-        
+        await e.delete()
         for i in range(counter):
             await asyncio.wait([ 
             e.respond(spam_message)])
