f3500bcc6538a8a3e875b412d77d236b1a1413dd
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-03-20 17:06:19 +0530

    Update calladmin.py

--- userbot/plugins/calladmin.py
+++ userbot/plugins/calladmin.py
@@ -14,7 +14,7 @@ async def _(event):
         reason = input_str
     else:
         reason = "Spam Spotted"
-    mentions = "@admin: **{reason}**"
+    mentions = "@admin: {reason}"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f"[\u2063](tg://user?id={x.id})"
