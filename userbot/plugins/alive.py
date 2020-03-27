3e8ef0029dda9e5e505ea65a2ad006ce212c3405
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-03-27 19:16:20 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,8 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Halto_Tha\n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     "[Deploy this userbot Now](https://github.com/japarmar/GujjuBot)")
+    await alive.edit("`IAS YAS Bano Desh Ko Sambhalo ^.^ \nJinda Hoon BC ! Gand mara\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+                     f"`Mera Maalik`: {DEFAULTUSER}\n"
+                     "`Telethon version: Lawda+Lahsun\nPython: Lawda+Lahsun+Adrak\nMera Asli Maalik:` @TechnoAyanOfficial\n"
+                     "`[Join Channel](t.me/TechnoAyanBoT For Latest Updates`")
