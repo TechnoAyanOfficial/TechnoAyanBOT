416d054183ff5411ba72a64926ad3c6405c0ee5b
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-03-28 08:35:53 +0530

    Update police.py

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -8,7 +8,7 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "GujjuBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TechnoAyanBoT"
 
 @borg.on(admin_cmd(pattern=r"police"))
 
@@ -37,7 +37,7 @@ async def _(event):
             "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
             "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
             "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
-            "[GujjuBot](https://www.github.com/japarmar/GujjuBot) **Police iz Here**"
+            "{DEFAULTUSER} **Police iz Here**"
 
  ]
 
