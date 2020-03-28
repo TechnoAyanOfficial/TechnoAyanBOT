65d5e00e9f0b343ff2a9be51d00ac993235df51b
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-03-28 07:59:35 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -43,7 +43,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To` @Halto_Tha `To Remove this hack..`"
+            "`Targeted Account Hacked...\n\nPay 69$ To Remove this hack..`"
         ]
 
     for i in animation_ttl:
