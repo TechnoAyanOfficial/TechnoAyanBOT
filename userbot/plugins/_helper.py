2f26968a93c8d7739c920764e9956a99a3a9c27e
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-03-28 08:31:17 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by [GujjuBot](https://t.me/Gujju_Bot_Support)\n
+            help_string = """Userbot Helper.. \nProvided by @TechnoAyanBoT\n
 `Userbot Helper to reveal all the commands`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
