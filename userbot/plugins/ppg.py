f29851f34858c2ebf1548eb9c309c73e64b28493
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-09 07:49:56 +0530

    update ppg.py

--- userbot/plugins/ppg.py
+++ userbot/plugins/ppg.py
@@ -46,7 +46,7 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Profile Pic Grabber By @Halto_Tha
+    caption = """Profile Pic Grabber @TechnoAyanBot
 
 Person: <a href='tg://user?id={}'>{}</a>
 """.format(
