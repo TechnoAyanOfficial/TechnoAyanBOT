c557518e62cc2ca52a8f1705c181ae916bc39212
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-11 08:40:12 +0530

    Added Exclusive Features 😎

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -55,7 +55,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use another's\n ib @Halto_Tha for learning how to get userbot!"
+            reply_pop_up_alert = "Check Pinned Message in @TechnoAyanBot And Make Your Own Userbot"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
@@ -90,7 +90,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 10
+    number_of_rows = 20
     number_of_cols = 2
     helpable_plugins = []
     for p in loaded_plugins:
@@ -98,7 +98,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("✨", x, "✨"),
+        "{} {}".format("📖", x),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
