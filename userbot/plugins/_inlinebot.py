66b89ea998f68d0cf935c03919e81242adeb023a
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-11 12:23:59 +0530

    Added Exclusive Features 😎

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -18,7 +18,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "🤖 @TechnoAyanBot Help",
-                text="{}\n💪Currently Loaded Plugins: {}".format(
+                text="{}\n🔢Currently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False
