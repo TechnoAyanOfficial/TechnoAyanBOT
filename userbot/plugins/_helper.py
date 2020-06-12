a59c3937cc257491640ea58e0e73cede43e4dcbd
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-12 10:10:31 +0530

    update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -46,11 +46,7 @@ async def cmd_list(event):
                 tgbotusername,
                 help_string
             )
-            await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
+            await results[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
             await event.delete()
 
 @borg.on(admin_cmd(pattern="syntax (.*)"))
