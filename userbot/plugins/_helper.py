989edbd92926fcc11dfd06a850ca7e76fb01df01
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-12 11:07:44 +0530

    update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -8,7 +8,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "📖 " + i + "\n"
+                string += "ℹ️ " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """🤖 **TechnoAyanBot Helper** """
+            help_string = """TechnoAyanBoT Helper"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
