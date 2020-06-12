0b392b177af2df3b3cf9f70ef56af50c2de705fa
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-12 08:26:11 +0530

    Added Exclusive Features 😎

--- userbot/exclusive.py
+++ userbot/exclusive.py
@@ -1,5 +1,19 @@
+# Copyright (C) 2020 by TechnoAyanOfficial@Github, < https://github.com/TechnoAyanOfficial >.
+#
+# This file is part of < https://github.com/TechnoAyanOfficial/TechnoAyanBOT > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TechnoAyanOfficial/TechnoAyanBOT/blob/Exclusive/LICENSE >
+#
+# All rights reserved.
+
 import os
 
 STICKER_PACK_NAME = os.environ.get("STICKER_PACK_NAME", "Creepy Stickers")
 
 ANIMATED_STICKER_PNAME = os.environ.get("ANIMATED_STICKER_PNAME", "Creepy Animated Stickers")
+
+AUTONAME = os.environ.get("AUTONAME", None)
+
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/TechnoAyanBoT/80")
+
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
