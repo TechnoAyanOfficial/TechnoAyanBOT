057b409d25cc66c58331234672fd0010269b42b1
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-06-12 10:51:48 +0530

    Added Exclusive Features 😎

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -8,7 +8,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "ℹ️ " + i + "\n"
+                string += "📖 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -37,8 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by @TechnoAyanBoT\n
-`Userbot Helper to reveal all the commands`"""
+            help_string = """🤖 **TechnoAyanBot Helper** """
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
